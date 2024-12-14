d5b75d736f524c237b5865dadd3aa09e2ecb5ef2
Randy Ridenour <rlridenour@gmail.com>
2024-12-14 11:17:31 -0600

    Add persist and activities.

--- init.el
+++ init.el
@@ -472,10 +472,25 @@ If there are only two windows, jump directly to the other window."
   :config
   (load "~/Dropbox/emacs/my-emacs-abbrev"))
 
-(use-package ace-window
+(use-package activities
+  :init
+  (activities-mode)
+  (activities-tabs-mode)
+  ;; Prevent `edebug' default bindings from interfering.
+  (setq edebug-inhibit-emacs-lisp-mode-bindings t)
   :general
-  ("M-O" #'ace-window)
+  ("C-x C-a C-n" #'activities-new)
+  ("C-x C-a C-d" #'activities-define)
+  ("C-x C-a C-a" #'activities-resume)
+  ("C-x C-a C-s" #'activities-suspend)
+  ("C-x C-a C-k" #'activities-kill)
+  ("C-x C-a RET" #'activities-switch)
+  ("C-x C-a b" #'activities-switch-buffer)
+  ("C-x C-a g" #'activities-revert)
+  ("C-x C-a l" #'activities-list)
   )
+
+(use-package persist)
 
 (use-package aggressive-indent
   :config
