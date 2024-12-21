d8cced804b06b382eacb8561942f5a9a5888ad0e
Randy Ridenour <rlridenour@gmail.com>
2024-12-21 07:17:37 -0600

    Embed YouTube videos.

--- init.el
+++ init.el
@@ -2160,6 +2160,27 @@ If there are only two windows, jump directly to the other window."
 </div>
 </div>")
 
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"440\""
+          " height=\"335\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
 (defun orgblog-load-conversion ()
     (interactive)
 (progn
