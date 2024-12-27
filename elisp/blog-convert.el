85824683b4c430195f357c3d46cc485e2e77ec77
Randy Ridenour <rlridenour@gmail.com>
2024-12-27 12:38:47 -0600

    Add emacs-lisp snippet folder.

--- elisp/blog-convert.el
+++ elisp/blog-convert.el
@@ -341,3 +341,264 @@ Tagged: ")
 (save-buffer))
 (kill-buffer)
 )
+
+;; Extract file name and tags
+
+(defun make-file-link ()
+  (interactive)
+(defvar blog-title)
+(beginning-of-buffer)
+(re-search-forward "TITLE: " nil t)
+(copy-region-as-kill (point) (line-end-position))
+(setq blog-title (current-kill 0 t))
+;; (insert blog-title)
+(defvar blog-filename)
+(setq blog-filename (f-filename (buffer-file-name)))
+(defvar blog-file-link)
+(setq blog-file-link (s-concat "[[file:" blog-filename "][" blog-title "]]")))
+
+(defun get-tags ()
+  (interactive)
+  (beginning-of-buffer)
+(while (re-search-forward "Tagged: " nil t)
+(copy-region-as-kill (point) (line-end-position))
+(defvar blog-tags)
+(setq blog-tags (current-kill 0 t)))
+  )
+
+(defun write-blog-tags-title ()
+  (interactive)
+(defvar blog-tags-title)
+(make-file-link)
+(get-tags)
+(setq blog-tags-title (s-concat blog-tags " " blog-file-link "\n"))
+(f-append-text blog-tags-title 'utf-8 "/Users/rlridenour/Desktop/blog-tag-links.txt")
+(setq blog-tags "")
+  )
+
+;; Convert tagline to links
+
+(defun make-aesthetics-links ()
+  (interactive)
+  (beginning-of-buffer)
+  (search-forward "Tagged: ")
+  (while  (search-forward "aesthetics" nil 1)
+    (replace-match "[[file:./aesthetics.org][Aesthetics]]")))
+
+(defun make-army-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "army" nil t)
+  (replace-match "[[file:./army.org][Army]]")))
+  
+
+(defun make-ct-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "critical-thinking" nil t)
+  (replace-match "[[file:./critical-thinking.org][Critical-Thinking]]")))
+  
+
+(defun make-emacs-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "emacs" nil t)
+  (replace-match "[[file:./emacs.org][Emacs]]")))
+  
+
+(defun make-ethics-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "ethics" nil t)
+  (replace-match "[[file:./ethics.org][Ethics]]")))
+  
+
+(defun make-latex-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "latex" nil t)
+  (replace-match "[[file:./latex.org][Latex]]")))
+  
+
+(defun make-logic-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "logic" nil t)
+  (replace-match "[[file:./logic.org][Logic]]")))
+  
+
+(defun make-mac-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "mac" nil t)
+  (replace-match "[[file:./mac.org][Mac]]")))
+  
+
+(defun make-misc-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "misc" nil t)
+  (replace-match "[[file:./misc.org][Misc]]")))
+  
+
+(defun make-music-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "music" nil t)
+  (replace-match "[[file:./music.org][Music]]")))
+  
+
+(defun make-org-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "org" nil t)
+  (replace-match "[[file:./org.org][Org]]")))
+  
+
+(defun make-philosophy-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "philosophy" nil t)
+  (replace-match "[[file:./philosophy.org][Philosophy]]")))
+  
+
+(defun make-poetry-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "poetry" nil t)
+  (replace-match "[[file:./poetry.org][Poetry]]")))
+  
+
+(defun make-politics-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "politics" nil t)
+  (replace-match "[[file:./politics.org][Politics]]")))
+  
+
+(defun make-prayer-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "prayer" nil t)
+  (replace-match "[[file:./prayer.org][Prayer]]")))
+  
+
+(defun make-quotes-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "quotes" nil t)
+  (replace-match "[[file:./quotes.org][Quotes]]")))
+  
+
+(defun make-religion-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "religion" nil t)
+  (replace-match "[[file:./religion.org][Religion]]")))
+  
+
+(defun make-shell-links ()
+  (interactive)
+  (beginning-of-buffer)
+  (search-forward "Tagged:")
+  (while (search-forward "shell" nil t)
+  (replace-match "[[file:./shell.org][Shell]]")))
+  
+
+(defun make-teaching-links ()
+  (interactive)
+    (beginning-of-buffer)
+  (search-forward "Tagged: ")
+(while (re-search-forward "teaching" nil t)
+  (replace-match "[[file:./teaching.org][Teaching]]")))
+
+;; Make tag link function
+
+(defun make-tag-links ()
+  (interactive)
+   (setq my-start (mark-marker))
+   (make-aesthetics-links)
+   (goto-char my-start)
+   (make-army-links)
+   (goto-char my-start)
+   (make-ct-links)
+   (goto-char my-start)
+   (make-emacs-links)
+   (goto-char my-start)
+   (make-ethics-links)
+   (goto-char my-start)
+   (make-latex-links)
+   (goto-char my-start)
+   (make-logic-links)
+   (goto-char my-start)
+   (make-mac-links)
+   (goto-char my-start)
+   (make-misc-links)
+   (goto-char my-start)
+   (make-music-links)
+   (goto-char my-start)
+   (make-org-links)
+   (goto-char my-start)
+   (make-philosophy-links)
+   (goto-char my-start)
+   (make-poetry-links)
+   (goto-char my-start)
+   (make-politics-links)
+   (goto-char my-start)
+   (make-prayer-links)
+   (goto-char my-start)
+   (make-quotes-links)
+   (goto-char my-start)
+   (make-religion-links)
+   (goto-char my-start)
+   (make-shell-links)
+   (goto-char my-start)
+   (make-teaching-links)
+)
+
+
+(pretty-hydra-define hydra-make-tag-links
+  (:color pink :quit-key "z" :title "Make Tag Links")
+  (" "
+   (("a" (make-aesthetics-links) "aesthetics")
+    ("A" (make-army-links) "army")
+    ("c" (make-ct-links) "ct")
+    ("e" (make-emacs-links) "emacs")
+    ("E" (make-ethics-links) "ethics")
+    ("l" (make-latex-links) "latex")
+    ("L" (make-logic-links) "logic")
+    ("M" (make-mac-links) "mac")
+    ("m" (make-misc-links) "misc")
+    ("o" (make-org-links) "org"))
+   " "
+    (("w" (make-philosophy-links) "philosophy")
+    ("v" (make-poetry-links) "poetry")
+    ("P" (make-politics-links) "politics")
+    ("p" (make-prayer-links) "prayer")
+    ("q" (make-quotes-links) "quotes")
+    ("r" (make-religion-links) "religion")
+    ("s" (make-shell-links) "shell")
+    ("t" (make-teaching-links) "teaching")
+    ("i" (insert "misc") "insert misc"))
+    ))
+
+
+
+(general-define-key
+ "H-l" #'hydra-make-tag-links/body)
