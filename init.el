5d112b77b4c5575a0db4a5182832f35bf5d31ecd
Randy Ridenour <rlridenour@gmail.com>
2024-12-28 07:12:28 -0600

    Add tag select functions.

--- init.el
+++ init.el
@@ -2194,6 +2194,55 @@ If there are only two windows, jump directly to the other window."
      (latex (format "\href{%s}{%s}"
 		    path (or desc "video"))))))
 
+(defun orgblog-all-tag-lines ()
+  "Get filetag lines from all posts."
+  (let ((post-dir (f-dirname (f-this-file)))
+	(regex "^#\\+filetags:\\s([a-zA-Z]+)"))
+    (shell-command-to-string
+     (concat "rg --context 0 --no-filename --no-heading --replace \"\\$1\" -- " (shell-quote-argument regex) " " post-dir))))
+
+(defun orgblog-all-tags ()
+  "Return a list of unique tags from all posts."
+  (delete-dups
+   (split-string (orgblog-all-tag-lines) nil t)))
+
+(defun orgblog-select-tag ()
+  "Select and insert a tag from tags in the blog."
+  (defvar newtag)
+  (setq newtag (completing-read "Tag: " (orgblog-all-tags))))
+
+(defun insert-post-tag ()
+  (orgblog-select-tag)
+  (beginning-of-buffer)
+  (search-forward "#+filetags" nil 1)
+  (end-of-line)
+  (insert (concat " " newtag))
+  (search-forward "Tagged:")
+  (end-of-line)
+  (insert (concat " [[file:../tags/" newtag ".org][" (s-titleized-words newtag) "]]")))
+
+(defun add-post-to-tagfile ()
+  (defvar tagfile)
+  (defvar post-filename)
+  (defvar post-title)
+  (setq tagfile (concat "../tags/" newtag ".org"))
+  (setq post-filename (f-filename (f-this-file)))
+  (progn
+    (beginning-of-buffer)
+    (search-forward "#+title: " nil 1)
+    (setq post-title (buffer-substring (point) (line-end-position))))
+  (when
+      (not (file-exists-p tagfile))
+    (f-append-text (concat "#+title: Tagged: " (s-titleized-words newtag) "\n#+setupfile: ../org-templates/post.org\n") 'utf-8 tagfile))
+  (f-append-text (concat "\n- [[file:../posts/" post-filename "][" post-title "]]") 'utf-8 tagfile))
+
+(defun new-orgblog-tag ()
+  (interactive)
+  (orgblog-select-tag)
+  (insert-post-tag)
+  (add-post-to-tagfile)
+  (save-buffer))
+
 (defun orgblog-load-conversion ()
   (interactive)
   (progn
