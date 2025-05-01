fae6ea655ed2e18034d777981b0b340810263a6c
Randy Ridenour <rlridenour@gmail.com>
2025-05-01 16:35:47 -0500

    Update some org settings.

--- init.el
+++ init.el
@@ -1704,6 +1704,11 @@ installed."
   (setq org-hide-leading-stars nil)
   (setq org-hide-emphasis-markers t)
   (setq org-list-indent-offset 2)
+  (setq org-agenda-skip-deadline-prewarning-if-scheduled t)
+
+  ;; Hide drawers
+  (setopt org-cycle-hide-drawer-startup t)
+  (setopt org-startup-folded 'nofold)
 
   (set-face-attribute 'org-level-1 nil :height 1.3 :weight 'bold :inherit 'fixed-pitch)
   (set-face-attribute 'org-level-2 nil :height 1.2 :weight 'bold :inherit 'fixed-pitch)
@@ -1729,16 +1734,16 @@ installed."
   (setq org-agenda-skip-scheduled-if-done t)
   (setq org-log-done t)
   (setq org-todo-keyword-faces
-	  '(("DONE" . "green4") ("TODO" . org-warning)))
+        '(("DONE" . "green4") ("TODO" . org-warning)))
   (setq org-agenda-files '("/Users/rlridenour/Library/Mobile Documents/iCloud~com~appsonthemove~beorg/Documents/org/"))
   (setq org-agenda-start-on-weekday nil)
   (setq org-agenda-window-setup 'current-window)
   (setq org-link-frame-setup
-	  '((vm . vm-visit-folder-other-frame)
-	(vm-imap . vm-visit-imap-folder-other-frame)
-	(gnus . org-gnus-no-new-news)
-	(file . find-file)
-	(wl . wl-other-frame)))
+        '((vm . vm-visit-folder-other-frame)
+  	(vm-imap . vm-visit-imap-folder-other-frame)
+  	(gnus . org-gnus-no-new-news)
+  	(file . find-file)
+  	(wl . wl-other-frame)))
   (require 'org-tempo)
   ;; Open directory links in Dired.
   (add-to-list 'org-file-apps '(directory . emacs)))
