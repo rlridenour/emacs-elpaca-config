95f02ee94903f15c5ec8c2c321de727e36f857a3
Randy Ridenour <rlridenour@gmail.com>
2025-07-26 08:58:56 -0500

    Make Mu4e use local time.

--- init.el
+++ init.el
@@ -2247,6 +2247,7 @@ installed."
   (setq mu4e-compose-format-flowed t)
   (setq mu4e-date-format "%y/%m/%d")
   (setq mu4e-headers-date-format "%Y/%m/%d")
+  (setq gnus-article-date-headers '(combined-local-lapsed)) ;; show local time in message header
   (setq mu4e-change-filenames-when-moving t)
   ;; customize the reply-quote-string
   (setq message-citation-line-format "On %a %d %b %Y at %R, %f wrote:\n")
@@ -3424,20 +3425,6 @@ installed."
 
 (setq default-directory "~/")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("77f281064ea1c8b14938866e21c4e51e4168e05db98863bd7430f1352cab294a"
-     default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; no-native-compile: t
