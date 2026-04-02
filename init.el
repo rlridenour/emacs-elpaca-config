68584dfb176e02676e62ea215339754df713fd34
Randy Ridenour <rlridenour@gmail.com>
2026-04-02 15:28:15 -0500

    Update mu version.

--- init.el
+++ init.el
@@ -2478,7 +2478,7 @@ installed."
 	  "C-," #'mu4e-sexp-at-point)
     :after org
     :init
-    (add-to-list 'load-path "/opt/homebrew/Cellar/mu/1.12.15/share/emacs/site-lisp/mu/mu4e/")
+    (add-to-list 'load-path "/opt/homebrew/Cellar/mu/1.14.0/share/emacs/site-lisp/mu/mu4e/")
     :config
     (setq mail-user-agent 'mu4e-user-agent)
     (setq mu4e-maildir "~/.maildir/")
